5a83e29aa919ed9dbd2d5d862e7bc232649e46e0
nader mohammed <nader.mohammed.20x@gmail.com>
2023-05-28 00:33:56 +0300

    employee

--- Components/Employees/EmployeeController.js
+++ Components/Employees/EmployeeController.js
@@ -1,10 +1,50 @@
-const Employee = require("./Employee");
+const Employee = require("./EmployeeSchema");
 
-module.exports.getAllEmployees = async (req, res, nex) => {
+module.exports.getAllEmployees = async (req, res, next) => {
   try {
     const employees = await Employee.find().exec();
     res.status(200).json({ status: "success", data: employees });
   } catch (error) {
-    res.status(400).json({ error });
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports.getEmployeeById = async (req, res, next) => {
+  try {
+    console.log(req.params.id);
+    const employee = await Employee.findById(req.params.id).exec();
+    res.status(200).json({ status: "success", data: employee });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports.createEmployee = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const employee = await Employee.create(req.body);
+    res.status(201).json({ status: "success", data: employee });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports.updateEmployee = async (req, res, next) => {
+  try {
+    const employee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).exec();
+    res.status(200).json({ status: "success", data: employee });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports.deleteEmployee = async (req, res, next) => {
+  try {
+    const employee = await Employee.findByIdAndDelete(req.params.id).exec();
+    res.status(200).json({ status: "success", data: employee });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
   }
 };
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
 const db = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 const port = process.env.PORT;
 
-mongoose
-  .createConnection(db)
-  .asPromise()
-  .then(() => {
-    console.log("DB Connected Succesffully");
+mongoose.connect(db).then(() => {
+  console.log("DB Connected Successfully");
 
-    // Run The Server
-    app.listen(port, () => {
-      console.log(`App Running on port ${port}`);
-    });
+  // Run The Server
+  app.listen(port, () => {
+    console.log(`App Running on port ${port}`);
   });
+});
 
 // Handle Unhandled Rejections
 process.on("unhandledRejection", (err) => {
