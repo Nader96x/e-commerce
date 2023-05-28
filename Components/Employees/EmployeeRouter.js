818b6c2054a640ac9597c2f24f5d86c4401ceeb2
nader mohammed <nader.mohammed.20x@gmail.com>
2023-05-28 21:12:59 +0300

    recover deleted files from github and setting Schema

--- Components/Employees/EmployeeRouter.js
+++ Components/Employees/EmployeeRouter.js
@@ -3,10 +3,15 @@ const EmployeeController = require("./EmployeeController");
 
 const EmployeeRouter = Router();
 
-EmployeeRouter.get("/", EmployeeController.getAllEmployees)
-  .get("/:id", EmployeeController.getEmployeeById)
-  .post("/", EmployeeController.createEmployee)
+EmployeeRouter.get("/", EmployeeController.getAllEmployees).post(
+  "/",
+  EmployeeController.createEmployee
+);
+
+EmployeeRouter.get("/:id", EmployeeController.getEmployeeById)
   .patch("/:id", EmployeeController.updateEmployee)
-  .delete("/:id", EmployeeController.deleteEmployee);
+  .delete("/:id", EmployeeController.deleteEmployee)
+  .post("/:id/ban", EmployeeController.ban)
+  .post("/:id/unban", EmployeeController.unban);
 
 module.exports = EmployeeRouter;
