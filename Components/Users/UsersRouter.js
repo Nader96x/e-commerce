3bddae0e1d178e70b270d9acb6af28bf75557121
hossamsalah22 <hossamsalahabbas@gmail.com>
2023-05-29 23:32:47 +0300

    Fixed Bug in Image on Update

--- Components/Users/UsersRouter.js
+++ Components/Users/UsersRouter.js
@@ -6,14 +6,21 @@ const upload = require("../../helpers/upload.helper");
 const router = express.Router();
 
 const assignImage = (req, res, next) => {
-  req.body.image = req.file.location;
+  if (req.file) {
+    req.body.image = req.file.location;
+  }
+  next();
+};
+
+const imageUpload = (req, res, next) => {
+  if (req.file) upload.single("image");
   next();
 };
 
 router
   .route("/")
   .get(usersController.getAllUsers)
-  .post(upload.single("image"), assignImage, usersController.createUser);
+  .post(imageUpload, assignImage, usersController.createUser);
 router
   .route("/:id")
   .get(usersController.getUser)
