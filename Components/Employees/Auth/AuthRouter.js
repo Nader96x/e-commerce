14959d14a7bb7dd3f69efac9958195f9eee9cf24
nader mohammed <nader.mohammed.20x@gmail.com>
2023-06-01 14:26:43 +0300

    test recaptcha

--- Components/Employees/Auth/AuthRouter.js
+++ Components/Employees/Auth/AuthRouter.js
@@ -1,10 +1,32 @@
 const { Router } = require("express");
 const AuthController = require("./AuthController");
+const Recaptcha = require("express-recaptcha").RecaptchaV3;
 
 const AuthRouter = Router();
 
-AuthRouter.get("/auth", AuthController.login);
-AuthRouter.post("/reset-password-token", AuthController.resetPasswordToken);
+const recaptcha = new Recaptcha(
+  process.env.RECAPTCHA_SITEKEY,
+  process.env.RECAPTCHA_SECRET
+);
+const recaptchaMW = (req, res, next) => {
+  console.log("recaptchaMW");
+  // if (process.env.NODE_ENV === "development") return next();
+  if (req.recaptcha.error) {
+    console.log("recaptchaMW error");
+    return res.status(400).json({ status: "fail", message: "Invalid Captcha" });
+  }
+  console.log("recaptchaMW success");
+
+  next();
+};
+
+AuthRouter.post("/auth", AuthController.login);
+AuthRouter.post(
+  "/reset-password-token",
+  recaptcha.middleware.verify,
+  recaptchaMW,
+  AuthController.resetPasswordToken
+);
 AuthRouter.post("/reset-password", AuthController.resetPassword);
 
 module.exports = AuthRouter;
