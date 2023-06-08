eb479b61fce08a026fdfcc517555a15170008f0d
hossamsalah22 <hossamsalahabbas@gmail.com>
2023-06-09 00:12:59 +0300

    Added User Auth for Dashboard

--- Components/Users/UsersRouter.js
+++ Components/Users/UsersRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const usersController = require("./UsersController");
 const addressesController = require("./AddressesController");
+const authController = require("./Auth/AuthController");
 const upload = require("../../helpers/upload.helper");
 
 const router = express.Router();
@@ -11,10 +12,22 @@ const assignImage = (req, res, next) => {
   }
   next();
 };
+
+router.post(
+  "/register",
+  upload.single("image"),
+  assignImage,
+  authController.signup
+);
+
+router.post("/login", authController.login);
+router.post("/forgot-password", authController.forgotPassword);
+router.patch("/reset-password/:token", authController.resetPassword);
+
 // User CRUD Routes
 router
   .route("/")
-  .get(usersController.getAllUsers)
+  .get(authController.protect, usersController.getAllUsers)
   .post(upload.single("image"), assignImage, usersController.createUser);
 router
   .route("/:id")
