38b64133aeaf6ae304be4105db344f648500188c
nader mohammed <nader.mohammed.20x@gmail.com>
2023-06-13 22:28:30 +0300

    banners

--- Routes/Website.js
+++ Routes/Website.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const ProductsRouter = require("../Components/Products/ProductRouterPublic");
 const CategoriesRouter = require("../Components/Categories/CategoriesRouterPublic");
+const SettingsRouter = require("../Components/Setting/SettingsRouterPublic");
 
 const Router = express.Router();
 Router.use("/products", ProductsRouter);
 Router.use("/categories", CategoriesRouter);
 
+Router.use("/", SettingsRouter);
 module.exports = Router;
