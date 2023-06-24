9f32d212d7dd7e8197aa018e0b72c1276fd187a1
nader mohammed <nader.mohammed.20x@gmail.com>
2023-06-24 17:32:11 +0300

    get public products by category with pagination

--- Components/Categories/CategoriesRouterPublic.js
+++ Components/Categories/CategoriesRouterPublic.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const CategoriesController = require("./CategoriesController");
+const Category = require("./Category");
+const ApiError = require("../../Utils/ApiError");
+
+const filterActive = (req, res, next) => {
+  req.query.is_active = true;
+  next();
+};
+
+const filterByCat = async (req, res, next) => {
+  const cat = await Category.findOne({ slug: req.params.slug }).exec();
+  if (!cat) {
+    return next(new ApiError("No Category Found", 400));
+  }
+  req.query.category_id = cat._id;
+  req.query.is_active = true;
+  next();
+};
 
 const router = express.Router();
 
+router.get("/", filterActive, CategoriesController.getPublicCategories);
+// router.get("/:slug/products", CategoriesController.getProductsByCategory);
 router.get(
-  "/",
-  (req, res, next) => {
-    req.query.is_active = true;
-    next();
-  },
-  CategoriesController.getPublicCategories
+  "/:slug/products",
+  filterByCat,
+  CategoriesController.getPublicProducts
 );
-router.get("/:slug/products", CategoriesController.getProductsByCategory);
 router.get("/:slug", CategoriesController.getOneCategoryBySlug);
 
 module.exports = router;
